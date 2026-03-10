494c48f06bfa52751a3bfd310719266f9da0de5c
Abhay Pratap Singh <abhay_pratap01@outlook.com>
2026-03-10 15:46:17 +0530

    commit

--- Jenkins/src/Hi.java
+++ Jenkins/src/Hi.java
@@ -3,5 +3,6 @@ public class Hi {
     static void main() {
         System.out.println("Hi");
         System.out.println("With an interval of 10 minutes");
+        System.out.println("Working");
     }
 }
